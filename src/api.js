070a82424071c77259a8751be1ef6654fa967622
Prattay9 <141072558+Prattay9@users.noreply.github.com>
2024-11-16 15:22:44 +0530

    Delete src directory

--- src/api.js
+++
@@ -1,8 +0,0 @@
-// src/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://backend-data-pearl.vercel.app/api', // Use the server's base URL
-});
-
-export default api;
